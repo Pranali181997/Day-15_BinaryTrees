54066b3ec772ef9f99e2546b1a582122d4993b1f
Pranali <96486431+Pranali181997@users.noreply.github.com>
2022-02-27 17:22:46 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,12 +11,12 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to Binary Search Tree!");
-            //Creating an object of bst
-            //BinarySearchTree<int> bst = new BinarySearchTree<int>();
-            //bst.Insert(56);
-            //bst.Insert(30);
-            //bst.Insert(70);
-            //bst.DisplayBST(bst.root);
+            Creating an object of bst
+            BinarySearchTree<int> bst = new BinarySearchTree<int>();
+            bst.Insert(56);
+            bst.Insert(30);
+            bst.Insert(70);
+            bst.DisplayBST(bst.root);
         }
     }
 }
